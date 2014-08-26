118631c47530ebbc6e9e0d6ef87239205ec86ada
rjhealy <rjh32009@gmail.com>
2014-08-26 11:46:29 -0400

    relax relax
    intial commit

---
+++ _js/_vti_cnf/bootstrap.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|31 Oct 2012 02:38:20 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|HtmlPage.html download.html
---
+++ _js/_vti_cnf/gen_validatorv4.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|27 Nov 2012 01:46:10 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Joe-PC\\Joe
+vti_modifiedby:SR|Joe-PC\\Joe
+vti_timecreated:TR|01 Jan 2011 00:27:52 -0000
+vti_backlinkinfo:VX|HtmlPage.html download.html
+vti_cacheddtm:TX|27 Nov 2012 01:46:10 -0000
+vti_filesize:IR|32085
+vti_nexttolasttimemodified:TR|01 Jan 2011 00:27:52 -0000
